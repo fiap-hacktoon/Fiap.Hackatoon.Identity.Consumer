55a81635bc6f31d9b9e3f1b79e400f7bb1fda561
rodrigo mahlow <rmahlow@gmail.com>
2025-05-07 21:28:47 -0300

    add Rabbitmq deployment and services

--- ContactsConsult.Api/Kubernets/deploy.sh
+++ ContactsConsult.Api/Kubernets/deploy.sh
@@ -20,4 +20,10 @@ kubectl apply -f Deployment.yaml  # Aplica o Deployment da aplicação
 echo "🚪 Aplicando Service da aplicação..."
 kubectl apply -f Service.yaml  # Aplica o Service da aplicação
 
+echo "🚪 Aplicando Rabbitmq Deployment.."
+kubectl apply -f RabbitmqDeployment.yaml  # Aplica o Service da aplicação
+
+echo "🚪 Aplicando Rabbitmq Service..."
+kubectl apply -f RabbitmqService.yaml  # Aplica o Service da aplicação
+
 echo "✅ Tudo aplicado com sucesso!"
